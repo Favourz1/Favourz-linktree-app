3aa140ed4407e60cab115fe4300ab8a02ffa5cb9
Favour Okoh <okohfavour91@gmail.com>
2022-11-02 12:25:05 +0100

    Fix: Corrections to the UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,15 @@ import ingressiveLogo from '../assets/ingressive-logo.png'
 export default function Footer() {
     return(
         <footer className="justify-content-center justify-content-sm-between">
-            <img className="col-12 col-sm" src={zuriLogo} alt="Zuri Internship Logo" />
-            <div className="col-12 col-sm footer-text">Hng internship 9 Frontend Task.</div>
-            <img className="col-12 col-sm" src={ingressiveLogo} alt="Ingressive for Good Logo" />
+            <div className="col-12 col-sm">
+                <img  src={zuriLogo} alt="Zuri Internship Logo" />
+            </div>
+            <div className="col-12 col-sm">
+                <div className="footer-text">Hng internship 9 Frontend Task.</div>
+            </div>
+          <div className="col-12 col-sm">
+              <img  src={ingressiveLogo} alt="Ingressive for Good Logo" />
+          </div>
         </footer>
     )
 }
